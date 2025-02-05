627780e9cacbef94fbc63f6218afe400db465cbe
George Asiedu <asiedug41@gmail.com>
2025-02-05 16:44:13 +0000

    helper functions exported

--- src/app/utils/query-params.ts
+++ src/app/utils/query-params.ts
@@ -2,13 +2,13 @@ import {inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {filter, map} from 'rxjs';
 
-const getTokenParam = () => {
+export const getTokenParam = () => {
   return inject(ActivatedRoute).queryParams.pipe(
     map(param => param['token'])
   );
 };
 
-const getIdParam = () => {
+export const getIdParam = () => {
   return inject(ActivatedRoute).paramMap.pipe(
     map(paramMap => paramMap.get('id')),
     filter(id => id !== null),
