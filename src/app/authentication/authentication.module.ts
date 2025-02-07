c244d75aa41ca89f838c3d761102c8dcf040988a
George Asiedu <asiedug41@gmail.com>
2025-02-07 14:45:14 +0000

    ForgotPasswordComponent added o mudule

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -7,13 +7,15 @@ import { RouterLink } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import {NgxSpinnerModule} from 'ngx-spinner';
 import { VerifyAccountComponent } from './verify-account/verify-account.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
   declarations: [
     SignupComponent,
     SigninComponent,
-    VerifyAccountComponent
+    VerifyAccountComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     CommonModule,
