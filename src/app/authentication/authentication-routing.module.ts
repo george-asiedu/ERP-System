6bdfec29ec6dde98005ad5f44a7552c7e38d24bc
George Asiedu <asiedug41@gmail.com>
2025-02-07 15:15:27 +0000

    'reset-password' route path configured

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -4,12 +4,14 @@ import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import {VerifyAccountComponent} from './verify-account/verify-account.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import {ResetPasswordComponent} from './reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: '', component: SigninComponent, title: 'ERP System | Sign in' },
   { path: 'signup', component: SignupComponent, title: 'ERP System | Signup' },
-  { path: 'verify-account', component: VerifyAccountComponent, title: 'ERP System | VerifyAccount' },
-  { path: 'forgot-password', component: ForgotPasswordComponent, title: 'ERP System | ForgotPassword' },
+  { path: 'verify-account', component: VerifyAccountComponent, title: 'ERP System | Verify Account' },
+  { path: 'forgot-password', component: ForgotPasswordComponent, title: 'ERP System | Forgot Password' },
+  { path: 'reset-password', component: ResetPasswordComponent, title: 'ERP System | Reset Password' },
 ];
 
 @NgModule({
