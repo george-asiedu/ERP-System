70d4c5d508a3d05f766b992fb321d003ef9a723c
George Asiedu <asiedug41@gmail.com>
2025-02-07 16:28:21 +0000

    congratulations component created

---
+++ src/app/shared/congratulations/congratulations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-congratulations',
+  standalone: false,
+  
+  templateUrl: './congratulations.component.html',
+  styleUrl: './congratulations.component.scss'
+})
+export class CongratulationsComponent {
+
+}
