93ff23889efba91a308d9e91dff72e8c1070276d
George Asiedu <asiedug41@gmail.com>
2025-02-07 14:44:58 +0000

    'forgot-password' route path configured

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import {VerifyAccountComponent} from './verify-account/verify-account.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', component: SigninComponent, title: 'ERP System | Sign in' },
   { path: 'signup', component: SignupComponent, title: 'ERP System | Signup' },
-  { path: 'verify-account', component: VerifyAccountComponent, title: 'ERP System | VerifyAccount' }
+  { path: 'verify-account', component: VerifyAccountComponent, title: 'ERP System | VerifyAccount' },
+  { path: 'forgot-password', component: ForgotPasswordComponent, title: 'ERP System | ForgotPassword' },
 ];
 
 @NgModule({
