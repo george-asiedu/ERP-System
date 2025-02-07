d2ca9395e113872df43af2bf0a8c72c6ecbe09ae
George Asiedu <asiedug41@gmail.com>
2025-02-07 15:59:36 +0000

    api endpoint updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  localUrl: 'http://localhost:3000',
+  localUrl: 'http://localhost:3000/api/',
   productionUrl: 'https://erp-system-hr-module.onrender.com/api/'
 };
