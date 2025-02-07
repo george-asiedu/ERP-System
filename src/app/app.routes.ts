da78ca4011c0600ed8b326e9a9662af696bb100d
George Asiedu <asiedug41@gmail.com>
2025-02-07 16:27:02 +0000

    congratulations route path configured with canActivate guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+import {authGuard} from './guard/auth.guard';
 
 export const routes: Routes = [
-  { path: '', 
+  { path: '',
     loadChildren: () => import('./authentication/authentication.module')
-      .then(m => m.AuthenticationModule) 
+      .then(m => m.AuthenticationModule)
+  },
+  { path: 'congratulations',
+    loadChildren: () => import('./shared/shared.module')
+      .then(m => m.SharedModule),
+    canActivate: [authGuard]
   }
 ];
