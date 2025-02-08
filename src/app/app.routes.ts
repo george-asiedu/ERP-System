5dfcff5e0cc680763f40308a57caa8277b8daf34
George Asiedu <asiedug41@gmail.com>
2025-02-08 17:05:18 +0000

    RBAC added to admin route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import {authGuard} from './guard/auth.guard';
+import { Roles } from './model/authentication';
 
 export const routes: Routes = [
   { path: '',
@@ -14,6 +15,7 @@ export const routes: Routes = [
   { path: 'admin',
     loadChildren: () => import('./admin/admin.module')
       .then(m => m.AdminModule),
-    canActivate: [authGuard]
+    canActivate: [authGuard],
+    data: { roles: [Roles.Admin] }
   },
 ];
